a9b5f4695381987306edd729b7edff3ad390ea60
ivars-vids <ivars.vids@gmail.com>
2022-03-14 14:09:43 +0200

    Add files via upload

---
+++ 2022/1337CTF/Binomial Ways/challenge.py
@@ -0,0 +1,27 @@
+##from secret import flag
+flag = "27F;VP@bAs>clu}={9ln=_o1{0n5tp~"
+val = []
+flag_length = len(flag)
+print(flag_length)
+
+def factorial(n):
+    f = 1
+    for i in range(2, n+1):
+        f *= i
+    return f
+
+def series(A, X, n):
+    nFact = factorial(n)
+    for i in range(0, n + 1):
+        niFact = factorial(n - i)
+        iFact = factorial(i)
+        aPow = pow(A, n - i)
+        xPow = pow(X, i)
+        val.append(int((nFact * aPow * xPow) / (niFact * iFact)))
+
+A = 1; X = 1; n = 30
+series(A, X, n)
+ct = []
+for i in range(len(flag)):
+    ct.append(chr(ord(flag[i])-val[i]%26))
+print(''.join(ct))
